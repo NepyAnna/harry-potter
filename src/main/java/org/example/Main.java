e09296d3e0ff78e06c7815f2795a09626e1d0983
NepyAnna <nepyann@gmail.com>
2025-05-08 14:02:57 +0200

    added use cases to main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,15 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.models.Character;
+import org.example.models.Professor;
+import org.example.models.Student;
+
 public class Main {
     public static void main(String[] args) {
+        Character harry = new Student("Harry Potter", "Gryffindor", 5);
+        Character snape = new Professor("Severus Snape", "Slytherin", "Potions");
 
+        System.out.println(harry.introduce());
+        System.out.println(snape.introduce());
     }
 }
